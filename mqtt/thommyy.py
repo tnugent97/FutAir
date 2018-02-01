046cff4ba2785c89cdca73387548ba1759607442
tnugent97 <thomasnugent@me.com>
2018-02-01 09:57:07 +0000

    mqtt working

--- mqtt/thommyy.py
+++ mqtt/thommyy.py
@@ -1,9 +1,18 @@
 from umqtt.simple import MQTTClient
+import network
+import machine
 
 def main(server="192.168.0.10"):
-    c = MQTTClient("umqtt_client", server)
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(False)
+
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect('EEERover','exhibition')
+
+    c = MQTTClient(machine.unique_id(), server)
     c.connect()
-    c.publish(b"esys/Thom&Doug", b"hello")
+    c.publish(b"esys/Thom&Doug/test", bytes("hello", 'utf-8'))
     c.disconnect()
 
 if __name__ == "__main__":
