9ade723e46afb9dae6f41056b3cb432900cb1bf7
George Punter <punter.s.g@gmail.com>
2018-02-08 13:38:16 +0000

    [website] added map and hacked nice navbar. Added fake pins to map with data.

--- website/web/landing/views.py
+++ website/web/landing/views.py
@@ -34,3 +34,33 @@ def make_error_response(description):
 @landing.route('/home', methods=['GET'])
 def index():
     return render_template('index.html', page_title='Home')
+    
+
+@landing.route('/hub', methods=['GET'])
+def hub():
+    # Get data from database
+     
+    # Fake data for teesting
+    airheads = {
+        "uniqueid1":
+            {"location":{"lat":51.4988,"lng":-0.1749},
+             "payload":{
+                "temp":5, # degrees centigrade
+                "humidity":0.62, # Percentage
+                "CO conc.":10,
+                "NO2 conc.":50, # in microgrammes per metre cubed (ug/m3)
+                "pressure":1017.5 # hPa
+                }
+            },
+        "uniqueid2":
+            {"location":{"lat":51.5073,"lng":-0.1657},
+             "payload":{
+                "temp":5, # degrees centigrade
+                "humidity":0.62, # Percentage
+                "CO conc.":8,
+                "NO2 conc.":40, # in microgrammes per metre cubed (ug/m3)
+                "pressure":1012.5 # hPa
+                }
+            },
+        }
+    return render_template('hub.html', page_title='Data Hub', data = airheads)
