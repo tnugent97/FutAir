148ab154c055806798b3bc5d8831ffaf7ba2481a
Doug Brion <db1415@ic.ac.uk>
2018-02-09 17:10:26 +0000

    Paho with JSON works

--- src/mqtt/mqtt_test.py
+++ src/mqtt/mqtt_test.py
@@ -1,6 +1,7 @@
 from umqtt.simple import MQTTClient
 import network
 import machine
+import json
 
 def main(server="192.168.0.10"):
     ap_if = network.WLAN(network.AP_IF)
@@ -10,9 +11,14 @@ def main(server="192.168.0.10"):
     sta_if.active(True)
     sta_if.connect('EEERover','exhibition')
 
+    send_msg = {
+        'data_to_send': "heloo",
+        'also_send_this': 56
+    }
+
     c = MQTTClient(machine.unique_id(), server)
     c.connect()
-    c.publish(b"esys/Thom&Doug/test", bytes("hello", 'utf-8'))
+    c.publish(b"esys/Thom&Doug/test", bytes(json.dumps(send_msg), 'utf-8'))
     c.disconnect()
 
 if __name__ == "__main__":
