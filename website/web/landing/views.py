59c7895bce258c0bbd2c57c328a5680832aa3008
George Punter <punter.s.g@gmail.com>
2018-02-13 12:50:47 +0000

    Fixed nav on hub.html. Added cool map circles and colours.

--- website/web/landing/views.py
+++ website/web/landing/views.py
@@ -4,7 +4,8 @@ import os
 import datetime
 import flask
 import logging
-import sqlite3
+import json
+#import sqlite3
 
 from flask import (
     #Flask,
@@ -36,54 +37,66 @@ def make_error_response(description):
 def index():
     return render_template('index.html', page_title='Home')
 
+MQTT_DB = 'web/db/mqtt.json'
+IORA_DB = 'web/db/iora.json'
 
-DATABASE = 'web/db/history.db'
-def make_dicts(cursor, row):
-    return dict((cursor.description[idx][0], value)
-                for idx, value in enumerate(row))
-def get_db():
-    db = sqlite3.connect(DATABASE)
-    db.row_factory = make_dicts
-    return db
-        
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-
-def insert_db(values=(1,"hi", 0.4,0.3,43,0.65,0.2,0.3,100,datetime.datetime.now(),datetime.datetime.now())):
-    # id,nickname,lat,lng,temp,humidity,CO_conc,NO2,pressure,device_time,created
-    cur = get_db().cursor()
-    cur.execute('INSERT INTO datapoints VALUES (?,?,?,?,?,?,?,?,?,?,?)', values)
-    
 @landing.route('/hub', methods=['GET'])
-def hub():
-    # Get data from database
-    # insert_db()
-    #data = query_db('SELECT * FROM datapoints')
+@landing.route('/hub/{<string:meth>}', methods=['GET'])
+def hub(meth="none"):
+    try:
+        # Get data from database
+        if (meth.lower() == "iora"):
+            futair_data = json.load(open(IORA_DB))
+        
+        else:
+            futair_data = json.load(open(MQTT_DB))
+    except:
+        futair_data = {
+            "uniqueid1":
+                {"location":{"lat":51.4988,"lng":-0.1749},
+                 "payload":{
+                    "temp":5, # degrees centigrade
+                    "humidity":0.62, # Percentage
+                    "CO":7,
+                    "NO2":50, # in microgrammes per metre cubed (ug/m3)
+                    "pressure":1017.5 # hPa
+                    }
+                },
+            "uniqueid2":
+                {"location":{"lat":51.5073,"lng":-0.1657},
+                 "payload":{
+                    "temp":5, # degrees centigrade
+                    "humidity":0.62, # Percentage
+                    "CO":5,
+                    "NO2":40, # in microgrammes per metre cubed (ug/m3)
+                    "pressure":1012.5 # hPa
+                    }
+                },
+            }
     
+    # NO2: 45-70, 53 is standard
+    # CO: 
     # Fake data for teesting
-    airheads = {
-        "uniqueid1":
-            {"location":{"lat":51.4988,"lng":-0.1749},
-             "payload":{
-                "temp":5, # degrees centigrade
-                "humidity":0.62, # Percentage
-                "CO conc.":10,
-                "NO2 conc.":50, # in microgrammes per metre cubed (ug/m3)
-                "pressure":1017.5 # hPa
-                }
-            },
-        "uniqueid2":
-            {"location":{"lat":51.5073,"lng":-0.1657},
-             "payload":{
-                "temp":5, # degrees centigrade
-                "humidity":0.62, # Percentage
-                "CO conc.":8,
-                "NO2 conc.":40, # in microgrammes per metre cubed (ug/m3)
-                "pressure":1012.5 # hPa
-                }
-            },
-        }
-    return render_template('hub.html', page_title='Data Hub', data = airheads)
+    
+    return render_template('hub.html', page_title='Data Hub', data = futair_data)
+
+
+#DATABASE = 'web/db/history.db'
+#def make_dicts(cursor, row):
+#    return dict((cursor.description[idx][0], value)
+#                for idx, value in enumerate(row))
+#def get_db():
+#    db = sqlite3.connect(DATABASE)
+#    db.row_factory = make_dicts
+#    return db
+#        
+#def query_db(query, args=(), one=False):
+#    cur = get_db().execute(query, args)
+#    rv = cur.fetchall()
+#    cur.close()
+#    return (rv[0] if rv else None) if one else rv
+#
+#def insert_db(values=(1,"hi", 0.4,0.3,43,0.65,0.2,0.3,100,datetime.datetime.now(),datetime.datetime.now())):
+#    # id,nickname,lat,lng,temp,humidity,CO_conc,NO2,pressure,device_time,created
+#    cur = get_db().cursor()
+#    cur.execute('INSERT INTO datapoints VALUES (?,?,?,?,?,?,?,?,?,?,?)', values)
